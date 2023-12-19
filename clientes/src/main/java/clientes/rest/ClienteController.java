a6a2a363b8fde93c698531ac95b7125b1eecb0ac
marcus aurelio <marcusaurelio743@gmail.com>
2023-12-19 16:00:58 -0300

    atualizando projeto

--- clientes/src/main/java/clientes/rest/ClienteController.java
+++ clientes/src/main/java/clientes/rest/ClienteController.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -51,6 +53,38 @@ public class ClienteController {
 		}
 		
 		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping("{id}")
+	public ResponseEntity<?> deletar(@PathVariable Long id) {
+		Optional<Cliente> clienteExistente = repository.findById(id);
+		
+		if(clienteExistente.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}else {
+			repository.deleteById(id);
+			return ResponseEntity.noContent().build();
+		}
+		
+	}
+	
+	@PutMapping("{id}")
+	
+	public ResponseEntity<?> atualizar(@PathVariable Long id, @RequestBody Cliente clienteAtualizado){
+		
+		Optional<Cliente> clienteExistente = repository.findById(id);
+		
+		if(clienteExistente.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}else {
+			clienteExistente.get().setNome(clienteAtualizado.getNome());
+			clienteExistente.get().setCpf(clienteAtualizado.getCpf());
+			
+			repository.save(clienteExistente.get());
+			
+			return ResponseEntity.noContent().build();
+		}
+		
 	}
 	
 
