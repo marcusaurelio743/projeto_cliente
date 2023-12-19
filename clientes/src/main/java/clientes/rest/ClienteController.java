a77134ce652f034a5918a6928734d849b28b83f0
marcus aurelio <marcusaurelio743@gmail.com>
2023-12-19 14:35:45 -0300

    criando metodo de salvar e testando usando o postman

--- clientes/src/main/java/clientes/rest/ClienteController.java
+++ clientes/src/main/java/clientes/rest/ClienteController.java
@@ -1,8 +1,14 @@
 package clientes.rest;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,8 +31,26 @@ public class ClienteController {
 	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Cliente salvar( Cliente cliente) {
+	public Cliente salvar( @RequestBody Cliente cliente) {
 		return repository.save(cliente);
+	}
+	
+	@GetMapping("{id}")
+	
+	public ResponseEntity<Cliente> getById( @PathVariable Long id) {
+		
+		Optional<Cliente> clienteExistente = repository.findById(id);
+		
+		Cliente cliente = new Cliente();
+		
+		if(clienteExistente.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		else {
+			cliente = clienteExistente.get();
+		}
+		
+		return ResponseEntity.ok(cliente);
 	}
 	
 
